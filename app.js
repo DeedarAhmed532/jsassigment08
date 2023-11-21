d5ed8445efd8325643436e1bd44f29125c293693
Deedar Ahmed <deedarahmed532@gmail.com>
2023-11-21 07:27:58 +0500

    0o

---
+++ app.js
@@ -0,0 +1,27 @@
+// ja var for num 
+
+var age = 22;
+alert ("i am "+age+" year old")
+
+
+
+var visit=0
+visit++
+alert("you have visit this site "+visit+" times")
+
+
+
+
+var birthYear = 2001
+document.write("my birth year is "+birthYear+" <br> data type of my declared varaible is number <br>")
+
+
+
+var visitorName = prompt ("visitor ; name" , " deedar" )
+document.write("visitor name is "+visitorName+" <br> ")
+var productTitle = prompt ("product title ")
+document.write("visitor product is "+productTitle+" <br>")
+var order = prompt ("how many product you order")
+document.write("visitor has "+order+" order <br>")
+
+
